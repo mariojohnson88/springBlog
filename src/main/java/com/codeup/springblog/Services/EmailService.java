3a4dc91c38f08d21a38198080bf402c77460bcd2
Mario Johnson <johnson.mario8404@gmail.com>
2019-09-12 16:53:51 -0500

    Completed Services Exercises

---
+++ src/main/java/com/codeup/springblog/Services/EmailService.java
@@ -0,0 +1,34 @@
+package com.codeup.springblog.Services;
+
+import com.codeup.springblog.models.Post;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service("mailService")
+public class EmailService {
+
+    @Autowired
+    public JavaMailSender emailSender;
+
+    @Value("${spring.mail.from}")
+    private String from;
+
+    public void prepareAndSend(Post post, String subject, String body){
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(from);
+        message.setTo(post.getOwner().getEmail());
+        message.setSubject(subject);
+        message.setText(body);
+
+        try {
+            this.emailSender.send(message);
+        }
+            catch (MailException ex) {
+            System.err.println(ex.getMessage());
+        }
+    }
+}
