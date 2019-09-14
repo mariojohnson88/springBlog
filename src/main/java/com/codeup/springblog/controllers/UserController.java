4bedb9bc1f6cbd05c53498d213baf8034546fd06
Mario Johnson <johnson.mario8404@gmail.com>
2019-09-14 17:00:59 -0500

    updated functionality

--- src/main/java/com/codeup/springblog/controllers/UserController.java
+++ src/main/java/com/codeup/springblog/controllers/UserController.java
@@ -1,13 +1,17 @@
 package com.codeup.springblog.controllers;
 
 
+import com.codeup.springblog.daos.PostRepository;
 import com.codeup.springblog.daos.UserRepository;
+import com.codeup.springblog.models.Post;
 import com.codeup.springblog.models.User;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -15,10 +19,12 @@ public class UserController {
 
     private UserRepository users;
     private PasswordEncoder passwordEncoder;
+    private PostRepository postRepository;
 
-    public UserController(UserRepository users, PasswordEncoder passwordEncoder) {
+    public UserController(UserRepository users, PasswordEncoder passwordEncoder, PostRepository postRepository) {
         this.users = users;
         this.passwordEncoder = passwordEncoder;
+        this.postRepository = postRepository;
     }
 
     @GetMapping("/sign-up")
@@ -33,5 +39,13 @@ public class UserController {
         user.setPassword(hash);
         users.save(user);
         return "redirect:/login";
+    }
+
+    @GetMapping("/users-posts")
+    public String ownerPosts(Model vModel) {
+        User userSession = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        Iterable<Post> posts = postRepository.findByOwner(userSession);
+        vModel.addAttribute("posts", posts);
+        return "users/users-posts";
     }
 }
--- src/main/java/com/codeup/springblog/daos/PostRepository.java
+++ src/main/java/com/codeup/springblog/daos/PostRepository.java
@@ -1,11 +1,12 @@
 package com.codeup.springblog.daos;
 
 import com.codeup.springblog.models.Post;
+import com.codeup.springblog.models.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
 public interface PostRepository extends CrudRepository<Post, Long> {
-
+    Iterable<Post>findByOwner(User user);
 }
